475b64f8c3189672a01509143a805e9bbbb964e8
Vladimir <popovvs86@gmail.com>
2015-02-01 19:53:38 +0300

    new
    new

--- UIPageControlWithImage/ViewController.m
+++ UIPageControlWithImage/ViewController.m
@@ -10,7 +10,6 @@
 
 @interface ViewController ()
 
-@property (nonatomic, strong) NSMutableArray * array;
 
 @end
 
@@ -29,37 +28,37 @@
 -(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
     
 //    NSLog(@"%f", scrollView.contentOffset.x);
-    
+//    
     page = scrollView.contentOffset.x/scrollView.frame.size.width;
     
     pageControl.currentPage = page;
-    
-    
-    if (pageControl.currentPage == 0) {
-        
-        label.textColor = [UIColor yellowColor];
-        UIFont *font = [UIFont fontWithName:@"Arial" size:20];
-        [label setFont:font];
-        
-        label.text = @"Страница 1";
-        
-    }
-    
-    if (pageControl.currentPage == 1) {
-        
-        label.textColor = [UIColor redColor];
-        UIFont *font = [UIFont fontWithName:@"Arial" size:25];
-        [label setFont:font];
-        label.text = @"Страница 2";
-    }
-    
-    if (pageControl.currentPage == 2) {
-        
-        label.textColor = [UIColor whiteColor];
-        UIFont *font = [UIFont fontWithName:@"Arial" size:30];
-        [label setFont:font];
-        label.text = @"Страница 3";
-    }
+//
+//    
+//    if (pageControl.currentPage == 0) {
+//        
+//        label.textColor = [UIColor yellowColor];
+//        UIFont *font = [UIFont fontWithName:@"Arial" size:20];
+//        [label setFont:font];
+//        
+//        label.text = @"Страница 1";
+//        
+//    }
+//    
+//    if (pageControl.currentPage == 1) {
+//        
+//        label.textColor = [UIColor redColor];
+//        UIFont *font = [UIFont fontWithName:@"Arial" size:25];
+//        [label setFont:font];
+//        label.text = @"Страница 2";
+//    }
+//    
+//    if (pageControl.currentPage == 2) {
+//        
+//        label.textColor = [UIColor whiteColor];
+//        UIFont *font = [UIFont fontWithName:@"Arial" size:30];
+//        [label setFont:font];
+//        label.text = @"Страница 3";
+//    }
     
     NSLog(@"%d", page);
     
@@ -68,27 +67,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.array = [NSMutableArray array];
+    NSArray * array_images = [[NSArray alloc]initWithObjects:@"1.jpg", @"2.jpg", @"3.jpg", nil];
+    NSArray * array_pages = [[NSArray alloc]initWithObjects:@"Страница 1", @"Страница 2", @"Страница 3", nil];
+    NSMutableArray * array_result = [NSMutableArray array];
+
+
     
     
-    for (int i =1; i<4; i++) {
+    for (int i = 0; i< array_images.count; i++) {
         
-        [self.array addObject:[UIImage imageNamed:[NSString stringWithFormat:@"%d.jpg", i]]];
+        NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
+        [array_images objectAtIndex:i],@"image_name",
+        [array_pages objectAtIndex:i],@"page_name",
+        nil];
+
+        
+        [array_result addObject:dict];
     }
     
     
-    
-    for (int i = 0; i<self.array.count; i++) {
+    for (int i = 0; i< array_result.count; i++) {
         
-        UIImageView *imagen = [[UIImageView alloc]initWithImage:[self.array objectAtIndex:i]];
-        
+        UIImageView *imagen = [[UIImageView alloc]init];
         imagen.frame = CGRectMake(i * 320, 0, 320, 200);
+        UIImage * image = [UIImage imageNamed:[NSString stringWithFormat:@"%@", [[array_result objectAtIndex:i]valueForKey:@"image_name"]]];
+        imagen.image = image;
+        
+        UILabel * page_name = [[UILabel alloc] initWithFrame:CGRectMake(0, 200 + 20, 320, 30)];
+        page_name.text = [NSString stringWithFormat:@"%@", [[array_result objectAtIndex:i]valueForKey:@"page_name"]];
+        page_name.textAlignment = NSTextAlignmentCenter;
+        page_name.textColor = [UIColor whiteColor];
+
 
         [imagen setContentMode:UIViewContentModeScaleAspectFit];
-        
+        [imagen addSubview:page_name];
         [scroller addSubview:imagen];
         
-        //        [self.view addSubview:label];
+      
         
     }
     
@@ -98,13 +113,13 @@
 //    [scroller setContentInset:UIEdgeInsetsMake(0, 0, 0, offset)];
 
     
-    scroller.contentSize = CGSizeMake(320* self.array.count, 200); // (320*3) - this number of pages for scroll
+    scroller.contentSize = CGSizeMake(320* array_result.count, 200); // (320*3) - this number of pages for scroll
     scroller.pagingEnabled = YES;
     
     [self.view addSubview:scroller];
     
     
-    pageControl.numberOfPages = 3;
+    pageControl.numberOfPages = array_result.count;
     pageControl.currentPage = 0;
     
 }
